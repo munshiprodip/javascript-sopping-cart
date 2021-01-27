431dc652a0c43948232749e21440ebd4854dbb19
Prodip M <munshiprodip@gmail.com>
2021-01-28 02:15:02 +0600

    Final Commit

--- script.js
+++ script.js
@@ -1,88 +1,51 @@
-// Add event litener to all button
-document.getElementById('btnPlus').addEventListener('click', ()=>{
-    updatePhoneQuantity(1)
-})
-
-document.getElementById('btnMinus').addEventListener('click', ()=>{
-    updatePhoneQuantity(-1)
-})
-
-document.getElementById('removePhone').addEventListener('click', function(){
-    removePhone(this)
-})
-
-document.getElementById('btnPlusCase').addEventListener('click', ()=>{
-    updateCaseQuantity(1)
-})
-
-document.getElementById('btnMinusCase').addEventListener('click', ()=>{
-    updateCaseQuantity(-1)
-})
-
-document.getElementById('removeCase').addEventListener('click', function(){
-    removePhone(this)
-})
-
-
-let removePhone = (x) =>{
-    x.parentElement.parentElement.parentElement.style.display="none"
-}
-
-// function for phone quantity update
-let updatePhoneQuantity = (quantity) => {
-    const phonePrice = 1219
-    let phoneQuantity = getValueToInt('phoneQuantity')
-    phoneQuantity = phoneQuantity + quantity
-    if(phoneQuantity > 0){
-        let totalPhonePrice = phoneQuantity * phonePrice
-        setValue('phoneQuantity', phoneQuantity)
-        setText('totalPhonePrice', totalPhonePrice)
-        updateTotalPrice(quantity*phonePrice)
-    }else{
-        alert("Minium product quantity 1, if you want to remove this item click 'X' button")
-    }
+// function for incrase or decrase product quantity
+let updateProductQuantity = (productName, quantity) => {
+    const productPrice = (productName=="phone")? 1219 : 59
     
-}
-
-// function for case quantity update
-let updateCaseQuantity = (quantity) => {
-    const casePrice = 59
-    let caseQuantity = getValueToInt('caseQuantity')
-    caseQuantity = caseQuantity + quantity
-    if(caseQuantity > 0){
-        let totalCasePrice = caseQuantity * casePrice
-        setValue('caseQuantity', caseQuantity)
-        setText('totalCasePrice', totalCasePrice)
-        updateTotalPrice(quantity*casePrice)
+    let productQuantity = getValueToInt(productName + 'Quantity')
+    productQuantity = productQuantity + quantity
+    if(productQuantity > 0){
+        let totalProductPrice = productQuantity * productPrice
+        setValue(productName + 'Quantity', productQuantity)
+        setText(productName + 'TotalPrice', totalProductPrice)
+        updateTotalPrice()
     }else{
         alert("Minium product quantity 1, if you want to remove this item click 'X' button")
     }
 }
 
-// function for total price update
-let updateTotalPrice = (price) => {
-    let subTotal = getTextToFloat('subTotal')
-    subTotal = subTotal + price
+// function to update total price
+let updateTotalPrice = () => {
+    let phonePrice = getValueToInt('phoneQuantity')*1219
+    let casePrice = getValueToInt('caseQuantity')*59
+    let subTotal = phonePrice + casePrice
     setText('subTotal', subTotal)
     setText('total', subTotal)
 }
 
-// function get input value to integer 
+// function to remove item from cart 
+let removeItem = (productName) =>{
+    setValue(productName + 'Quantity', 0)
+    document.querySelector('.'+productName).style.display="none"
+    updateTotalPrice()
+}
+
+// function to get input field value to integer 
 let getValueToInt = (id) =>{
     return parseInt(document.getElementById(id).value)
 }
 
-// function get html text to integer
+// function to get html text to integer
 let getTextToFloat = (id) =>{
     return parseFloat(document.getElementById(id).innerText)
 }
 
-// function set input value
+// function to set input field value
 let setValue = (id, value) =>{
     document.getElementById(id).value = value
 }
 
-// function set html text
+// function to set text in html element
 let setText = (id, value) =>{
     document.getElementById(id).innerText = value
 }
